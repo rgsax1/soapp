a983b48b485c98c4a3f97a2cdf2c543539d6d082
Rodrigo Salles <rgsa@outlook.com.br>
2023-11-07 17:53:02 -0300

    adding general

--- soapp-spring/src/main/java/flexnyl/com/br/backendsoapp/equipment/general/EquipmentGeneral.java
+++ soapp-spring/src/main/java/flexnyl/com/br/backendsoapp/equipment/general/EquipmentGeneral.java
@@ -1,4 +1,29 @@
 package flexnyl.com.br.backendsoapp.equipment.general;
 
-public class EquipmentGeneral {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "equipment_general_info")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class EquipmentGeneral{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "equipment_manufacturer")
+    private String equipmentManufacturer;
+
+    @Column(name = "equipment_model")
+    private String equipmentModel;
+
+    @Column(name = "description")
+    private String description;
+
 }
