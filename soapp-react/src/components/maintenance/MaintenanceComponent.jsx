00074ce90f19c8da2cb8717a79e986bd28d995d5
Rodrigo Salles <rgsa@outlook.com.br>
2023-11-07 10:31:38 -0300

    formating

--- soapp-react/src/components/maintenance/MaintenanceComponent.jsx
+++ soapp-react/src/components/maintenance/MaintenanceComponent.jsx
@@ -41,6 +41,20 @@ const MaintenanceComponent = () => {
         }
     };
 
+    
+    function isValidISO8601Date(dateString) {
+        // Verifique se a data corresponde ao formato AAAA-MM-DD
+        const isoDateRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+        if (!isoDateRegex.test(dateString)) {
+            return false;
+        }
+
+        // Agora, você pode tentar criar uma data com a string e verificar se ela é válida
+        const parsedDate = new Date(dateString);
+        return !isNaN(parsedDate.getTime());
+    }
+
 
     useEffect(() => {
         listUsers().then((response) => {
@@ -71,18 +85,6 @@ const MaintenanceComponent = () => {
     }, [id]);
 
 
-    function isValidISO8601Date(dateString) {
-        // Verifique se a data corresponde ao formato AAAA-MM-DD
-        const isoDateRegex = /^\d{4}-\d{2}-\d{2}$/;
-
-        if (!isoDateRegex.test(dateString)) {
-            return false;
-        }
-
-        // Agora, você pode tentar criar uma data com a string e verificar se ela é válida
-        const parsedDate = new Date(dateString);
-        return !isNaN(parsedDate.getTime());
-    }
 
     function saveOrUpdateMaintenance(e) {
         e.preventDefault();
