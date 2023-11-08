fc3d33206bcda2f4c7021c8558154813e70da218
Rodrigo Salles <rgsa@outlook.com.br>
2023-11-08 16:02:30 -0300

    Correcting Equipment General URL service

--- soapp-react/src/components/equipment/equipment-general/EquipmentGeneralService.js
+++ soapp-react/src/components/equipment/equipment-general/EquipmentGeneralService.js
@@ -4,8 +4,9 @@ const EQUIPMENTGENERAL_REST_API_BASE_URL = 'http://localhost:8080/api/v1/equipme
 
 export const listEquipmentGenerals = () => axios.get(EQUIPMENTGENERAL_REST_API_BASE_URL);
 export const createEquipmentGeneral = (equipmentGeneral) => axios.post(EQUIPMENTGENERAL_REST_API_BASE_URL, equipmentGeneral);
-export const getEquipmentGeneral = (equipmentGeneralId) => axios.get(EQUIPMENTGENERAL_REST_API_BASE_URL, + '/' + equipmentGeneralId);
+export const getEquipmentGeneral = (equipmentGeneralId) => axios.get(`${EQUIPMENTGENERAL_REST_API_BASE_URL}/${equipmentGeneralId}`);
 export const updateEquipmentGeneral = (equipmentGeneralId, equipmentGeneral) => axios.put(EQUIPMENTGENERAL_REST_API_BASE_URL + '/' + equipmentGeneralId, equipmentGeneral);
-export const deleteEquipmentGeneral = (equipmentGeneralId) => axios.delete (EQUIPMENTGENERAL_REST_API_BASE_URL + '/ ' + equipmentGeneralId);
+export const deleteEquipmentGeneral = (equipmentGeneralId) => axios.delete(`${EQUIPMENTGENERAL_REST_API_BASE_URL}/${equipmentGeneralId}`);
+
 
 
